ab0e6f614d8ccade20c51ac9ab05698c524b4caf
Aldo Rogelio Bonilla  Guerrero <abonilla@ia.com.mx>
2019-03-05 16:43:24 -0600

    Remove Pods

--- Example/Pods/Target Support Files/DestinyLib/DestinyLib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DestinyLib : NSObject
-@end
-@implementation PodsDummy_DestinyLib
-@end
--- Example/Pods/Target Support Files/Pods-DestinyLib_Tests/Pods-DestinyLib_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DestinyLib_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_DestinyLib_Tests
-@end
